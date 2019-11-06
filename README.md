0974cce1889d17a70ea26d672be2bbf75509e697
bartosz25 <bartkonieczny@yahoo.fr>
2019-11-06 09:11:23 +0100

    update code and readme

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ If you want to test the code, follow these steps:
 
 1. Download https://github.com/bartosz25/data-generator and execute `examples/local_filesystem` generator. 
 You can increase `percentage_incomplete_data=2, percentage_inconsistent_data=2` to have more invalid data.
-2. Create virtualenv and install the dependencies from requirements.txt *TODO: add command*
-3. Execute `cerberus_talk/data_validation_pipeline.py`
+2. Create virtualenv and install the dependencies from requirements.txt `pip install -r requirements.txt`
+3. Change the `input_file` variable in `cerberus_talk/data_validation_pipeline.py`. You can use wildcard 
+on the directory.
+4. Execute `cerberus_talk/data_validation_pipeline.py`
--- cerberus_talk/data_validation_pipeline.py
+++ cerberus_talk/data_validation_pipeline.py
@@ -1,8 +1,7 @@
 from collections import defaultdict
 
-import time
 from cerberus import Validator
-from cerberus.errors import SchemaErrorHandler, ValidationError, ErrorDefinition, BasicErrorHandler
+from cerberus.errors import SchemaErrorHandler, ErrorDefinition
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType, StructField, IntegerType, TimestampType, DecimalType
 
@@ -32,9 +31,9 @@ schema = {
             'lang': {'type': 'string', 'required': True, 'allowed': ['fr', 'pl', 'en', 'de']},
             # you don't need to create a custom validator for network_exists. You can use 'allowed'
             # but for the demo is useful to show the custom validator feature
-            'network': {'type': 'string', 'required': True,
-                        #'networkexists': set("adsl", "fiber_optic", "3g")
-                        }, # TODO: I removed  "4g" just to see the failures
+            # Here I removed the "4g" from the set just to see the failures produced by the
+            # custom validation rule
+            'network': {'type': 'string', 'required': True, 'network_exists': set(["adsl", "fiber_optic", "3g"])},
             'device': {
                 'type': 'dict',
                 'schema': {
@@ -48,8 +47,9 @@ schema = {
 
 UNKNOWN_NETWORK = ErrorDefinition(333, 'network_exists')
 
+
 class ExtendedValidator(Validator):
-    def _validate_networkexists(self, allowed_values, field, value):
+    def _validate_network_exists(self, allowed_values, field, value):
         # /home/bartosz/.virtualenvs/cerberus_talk/lib/python3.5/site-packages/cerberus/validator.py:1609: UserWarning: No validation schema is defined for the arguments of rule 'networkexists'
         #  "'%s'" % method_name.split('_', 2)[-1]
         if value not in allowed_values:
@@ -60,8 +60,6 @@ class ExtendedValidator(Validator):
 
 
 class ErrorCodesHandler(SchemaErrorHandler):
-#    messages = SchemaErrorHandler.messages.copy()
-#    messages[333] = "unallowed_network"
 
     def __call__(self, validation_errors):
         def concat_path(document_path):
@@ -77,15 +75,6 @@ class ErrorCodesHandler(SchemaErrorHandler):
             else:
                 output_errors[concat_path(error.document_path)] = error.code
         return output_errors
-data1 = {"user_id": 450081, "visit_id": "8f6ca65f40bf47bd863e6dfaa23ea985", "technical": {"device": {"version": "Samsung Galaxy Tab S3", "type": None}, "lang": "en", "browser": "Mozilla Firefox 52", "os": "Android 8.1", "network": '4g'}, "user": {"latitude": 30.5818, "longitude": -154.4656, "ip": "8395:9313:9b9e:4e62:26dd:7688:785:4a97"}, "source": {"site": "mysite.com", "api_version": "v3"}, "event_time": "2019-10-22T08:21:19+00:00", "page": {"previous": None, "current": "article category 14-5"}}
-data2 = {"user_id": 969491, "visit_id": "9a952bd082ac43679bc1cd8b8958a155", "technical": {"device": {"version": "Apple iPhone XS", "type": {"name": "smartphone"}}, "lang": None, "browser": {"language": "en", "name": "Mozilla Firefox 53"}, "os": "iOS 10", "network": "adsl"}, "user": {"latitude": -70.5207, "longitude": -166.6422, "ip": "bc1c:bed4:bc9f:72ef:13c0:777a:6747:5e17"}, "source": {"site": "partner2.com", "api_version": "v1"}, "event_time": "2019-10-22T08:21:19+00:00", "page": {"previous": None, "current": "article category 24-15"}}
-"""
-validator = ExtendedValidator(error_handler=ErrorCodesHandler()) #error_handler=ErrorCodesHandler())
-validation_result = validator.validate(data1, schema)
-for error_code, error_field in validator.errors.items():
-    print('Got {}={}'.format(error_code, error_field))
-#print('Got error code={} schema={} rule={} constraint={}'.format(error.code, error.schema_path, error.rule, error.constraint))
-"""
 
 
 def check_for_errors(rows):
@@ -97,15 +86,12 @@ def check_for_errors(rows):
     errors = defaultdict(default_dictionary)
     for row in rows:
         validator.clear_caches()
-        # I wish I could work directly on Row class but for now simply convert it recursively to a dictionary
-        #start = round(time.monotonic() * 1000)
-        # TODO: I'm testing here the version with normalize to False
-        # TODO: check what the normalize is doing
-        # TODO: time with normalize=False, 4 cores=3 minutes which is far better than 6 minutes with normalization turned on
-        # TODO: time with normalize=False, 4 cores, validator.clear_caches called every time=2 minutes  <== check if clear_caches has an impact?
+        # Note#1: I wish I could work directly on Row class but for now simply convert it recursively to a dictionary
+        # Note#2: I observed that disabling normalization, which is normal if you don't have any normalization
+        #         rule in your input schema, accelerates the processing time by factor of 2 (6 minutes --> 3 minutes)!
+        # Note#3: calling `recursive=True` is important here because otherwise any nested object won't be validated
+        #         correctly.
         validation_result = validator.validate(row.asDict(recursive=True), normalize=False)
-        #end = round(time.monotonic() * 1000)
-        #print("Executed#1 within {} ms".format((end - start)))
         # By default Cerberus returns a textual representation of the errors, like:
         # {'technical': [{'device': [{'version': ['null value not allowed']}]}], 'user': ['unknown field']}
         # It's hard to use because some errors can have different "codes" because they will store
@@ -118,20 +104,16 @@ def check_for_errors(rows):
                 # If you use a default handler, it will return a collection of strings for validator.errors
                 # like {'technical': [{'browser': ['null value not allowed'], 'device': [{'version': ['null value not allowed']}]}], 'source': [{'site': ['null value not allowed']}],
                 errors[error_field][error_code] += 1
-        #end = round(time.monotonic() * 1000)
-        #print("Executed#2 within {} ms".format((end - start)))
 
-    result = [(k, dict(v)) for k, v in errors.items()]
-    print('result={}'.format(result))
     return [(k, dict(v)) for k, v in errors.items()]
-
-# test on 3 cores, no .asDict but a hardcoded data1 ==> 8 minutes!
-# test on 3 cores, optimal version ==> 9 minutes for 99MB!
 
 # let's suppose that this schema comes from our data catalog and it's up to date
 # but it can be different from the validation schema since both pipelines (crawler and validator)
 # are separated. We could include them and, in that a case, implement crawling and validation
 # from a Kinesis or Kafka where the record is a JSON. It would be much harder to implement in batch
+# Note: I'm using here a PySpark SQL which involves an existence of the input schema. You could use here
+#       RDD abstraction directly as well. I chosen PySpark SQL though because it has a richer and easier API,
+#       at least for the input part.
 dataframe_schema = StructType(
     fields=[
         StructField("visit_id", StringType(), False),
@@ -163,34 +145,15 @@ def sum_errors_number(errors_count_1, errors_count_2):
 spark = SparkSession.builder.master("local[4]")\
     .appName("Python Spark SQL data validation with Cerberus").getOrCreate()
 
-errors_distribution = spark.read.json("/tmp/sessions/input/2019/10/22/08/d3c226d2-68db-45be-8a1b-8326aa59816e.json", schema=dataframe_schema, lineSep='\n')\
+# Set your file to validate here
+input_file = "/home/bartosz/tmp/test_generator/2019/08/11/09/9c67a3e6-5df4-49a4-b36d-65483297d7b0.json"
+
+# I can't use spark.read.json("/tmp/sessions/input/2019/10/22/07", lineSep='\n')****.toJSON()**** because
+# Cerberus works on the dictionaries. So converting a DataFrame into a JSON string is useless.
+errors_distribution = spark.read.json(input_file, schema=dataframe_schema, lineSep='\n')\
     .rdd\
     .mapPartitions(check_for_errors)\
     .reduceByKey(sum_errors_number)\
     .collectAsMap()
 
-
-print('errors_distribution={}'.format(errors_distribution))
-# TODO: see how to get extra fields from the validator
-"""
-Notes:
-I don't use DataFrame --> it has a schema and if you want to know the schema mismatches, like extra fields
-and so far, it can not be enough!  <==== I was wrong. I can use a DataFrame! The assumption here is that 
-I know the schema of my data but that schema is not necessarily the same as the schema used in the validation
-part. Eventually, if I discover a new field, the validation schema can be updated. 
-
-I can't use spark.read.json("/tmp/sessions/input/2019/10/22/07", lineSep='\n')****.toJSON()**** because
-Cerberus works on the dictionaries. So converting a DataFrame into a JSON string is useless.
-
-recursive=True is important!
-
-you can handle unknown fields with the flag
-"""
-
-#errors_distribution.show()
-
-"""
-    .rdd\
-    .mapPartitions(find_errors)\
-    .reduceByKey(sum_errors_number).collectAsMap()
-    """
+print('Got errors {}'.format(errors_distribution))
